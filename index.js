7fc5043828af10d108a6b661f840ac425e713daa
Paulo Salvatore <salvatoregames@gmail.com>
2021-05-04 12:34:52 -0300

    Atualização do endpoint para /hello

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
- 
-app.get('/', function (req, res) {
+
+app.get('/hello', function (req, res) {
   res.send('Hello World')
 })
- 
+
 app.listen(3000)
